0011ded4c09cbb7cba461b75c2f690042085b7ca
akhilkalyani <akhilkalyani7754@gmail.com>
2024-01-20 21:32:44 +0530

    BaseDialog added.

---
+++ Monobehaviour Classes/BaseDialog.cs
@@ -0,0 +1,10 @@
+using GF;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseDialog : MonoBehaviour
+{
+    public abstract void ShowDialog();
+    public abstract void CloseDialog();
+}
