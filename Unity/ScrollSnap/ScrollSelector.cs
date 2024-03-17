86d39db3b4be451a3a0297b83558f784cd5a9ef2
akhilkalyani <akhilkalyani7754@gmail.com>
2024-03-17 11:28:01 +0530

    simple one direction scroll snap

---
+++ Unity/ScrollSnap/ScrollSelector.cs
@@ -0,0 +1,40 @@
+﻿using SSR;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace SSR
+{
+    public class ScrollSelector : MonoBehaviour
+    {
+        private int _selectorPageIndex;
+        private ScrollSnapRect _scrollSnapInstance;
+        private Button button;
+        private GameObject _selector;
+        private void Awake()
+        {
+            _selector = transform.GetChild(0).gameObject;
+            button = transform.GetChild(1).GetComponent<Button>();
+            button.onClick.AddListener(() => SelectPage());
+            SetSelector(false);
+        }
+        public void SetScrollSnapInstance(ScrollSnapRect scroll_snap)
+        {
+            _scrollSnapInstance = scroll_snap;
+        }
+        public void SetPageIndex(int index)
+        {
+            _selectorPageIndex = index;
+        }
+        public void SetSelector(bool status)
+        {
+            _selector.SetActive(status);
+        }
+        private void SelectPage()
+        {
+            Debug.Log("Page-" + _selectorPageIndex);
+            _scrollSnapInstance.LerpToPage(_selectorPageIndex);
+        }
+    }
+}
